a5690af691988098d3f9efbfd144800fc8f07f73
diegomart <judimay18@gmail.com>
2015-03-17 19:34:42 -0600

    ListView usando WebView

---
+++ app/src/main/java/com/example/juandiego/listview/Item.java
@@ -0,0 +1,44 @@
+package com.example.juandiego.listview;
+
+public class Item {
+
+	private int image;
+	private String title;
+	private String url;
+
+	public Item() {
+		super();
+	}
+
+	public Item(int image, String title, String url) {
+		super();
+		this.image = image;
+		this.title = title;
+		this.url = url;
+	}
+
+	public int getImage() {
+		return image;
+	}
+
+	public void setImage(int image) {
+		this.image = image;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+}
